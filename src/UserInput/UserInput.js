d19421cdd50fecc66dafc21572c49b4015d5038d
Fraschetti <flaviofrasquetiferreira@fraschetti-3.local>
2017-11-08 13:10:24 -0200

    Added inline style as comment

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -3,10 +3,15 @@ import './UserInput.css'
 
 
 const UserInput = (props) => {
+    // For inline style
+    // const style {
+    //     border: '2px solid red'
+    // }
     return (
         <div className="UserInput">
             <input className="Input"
-            type="text" 
+            type="text"
+            /* style={style}  */
             placeholder="Type your new name here"
             onChange={props.changed} 
             value={props.currentName}/>
